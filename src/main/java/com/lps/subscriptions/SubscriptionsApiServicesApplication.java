41060ebce6283867b785b3108a1faa71ab8b72a4
rhonniel <rhonniel777@gmail.com>
2024-06-05 21:19:46 -0400

    first commit

---
+++ src/main/java/com/lps/subscriptions/SubscriptionsApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lps.subscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lps/subscriptions/SubscriptionsApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lps.subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
