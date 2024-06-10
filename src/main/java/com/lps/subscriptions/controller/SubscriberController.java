9d488c03909efbb2d6726b5a7e47b19b5bcddea6
rhonniel <rhonniel777@gmail.com>
2024-06-10 08:31:31 -0400

    create get service for all subscriber

--- src/main/java/com/lps/subscriptions/controller/SubscriberController.java
+++ src/main/java/com/lps/subscriptions/controller/SubscriberController.java
@@ -1,23 +1,25 @@
 package com.lps.subscriptions.controller;
 
-import com.lps.subscriptions.services.SubscriptionServices;
+import com.lps.subscriptions.model.Subscriber;
+import com.lps.subscriptions.services.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(value = {"*"}, exposedHeaders = {"Content-Disposition"})
-@RequestMapping(value = "/subscription")
-public class SubscriptionController {
-
+@RequestMapping(value = "/subscriber")
+public class SubscriberController {
     @Autowired
-    SubscriptionServices subscriptionServices;
+    SubscriberService subscriberService;
+
 
     @GetMapping
-    public String getAllSubscriber(){
-        return subscriptionServices.getHello();
+    public List<Subscriber> getAllSubscriber(){
+        return subscriberService.getAllSubscribers();
     }
-
 }
--- src/main/java/com/lps/subscriptions/model/Subscriber.java
+++ src/main/java/com/lps/subscriptions/model/Subscriber.java
@@ -1,2 +1,26 @@
-package com.lps.subscriptions.model;public class Subscriber {
+package com.lps.subscriptions.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Subscriber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column(name = "creation_date")
+    private Date creationDate;
+
 }
