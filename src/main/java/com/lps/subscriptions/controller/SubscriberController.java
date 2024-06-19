355658ebfeb5d2292d6da2da444a5ffcdebf11ee
rhonniel <rhonniel777@gmail.com>
2024-06-18 22:47:05 -0400

    Update controller with new security architecture

--- src/main/java/com/lps/subscriptions/controller/SubscriberController.java
+++ src/main/java/com/lps/subscriptions/controller/SubscriberController.java
@@ -2,16 +2,13 @@ package com.lps.subscriptions.controller;
 
 import com.lps.subscriptions.model.Subscriber;
 import com.lps.subscriptions.services.SubscriberService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(value = {"*"}, exposedHeaders = {"Content-Disposition"})
 @RequestMapping(value = "/subscriber")
 public class SubscriberController {
     @Autowired
@@ -20,6 +17,11 @@ public class SubscriberController {
 
     @GetMapping
     public List<Subscriber> getAllSubscriber(){
+
         return subscriberService.getAllSubscribers();
+    }
+    @PostMapping
+    public Subscriber createSubscriber(@Valid @RequestBody Subscriber subscriber){
+        return subscriberService.saveSubscriber(subscriber);
     }
 }
