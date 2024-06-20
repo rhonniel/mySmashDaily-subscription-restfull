215b1d9925fd22537c8c4ba8c897996ad4a4f275
rhonniel <rhonniel777@gmail.com>
2024-06-19 21:37:04 -0400

    fix pom.xml libs, el pegote branch lose this changes

--- src/main/java/com/lps/subscriptions/security/JwtTokenUtil.java
+++ src/main/java/com/lps/subscriptions/security/JwtTokenUtil.java
@@ -1,19 +1,13 @@
 package com.lps.subscriptions.security;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-
 @Component
 public class JwtTokenUtil {
 
