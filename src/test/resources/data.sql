a854862e88a524046e33cf38db8b5ed9853d7add
rhonniel <rhonniel777@gmail.com>
2024-06-20 23:46:28 -0400

    creating mock data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -10,3 +10,23 @@ INSERT INTO subscriber (name, email) VALUES ('John4 Doe', 'john4.doe@example.com
 INSERT INTO subscriber (name, email) VALUES ('Jane4 Doe', 'jane4.doe@example.com');
 INSERT INTO subscriber (name, email) VALUES ('John5 Doe', 'john5.doe@example.com');
 INSERT INTO subscriber (name, email) VALUES ('Jane5 Doe', 'jane5.doe@example.com');
+
+
+INSERT INTO subscription_type (name) VALUES ('Tournament Tracker');
+INSERT INTO subscription_type (name) VALUES ('Favorite Player');
+
+
+INSERT INTO subscription (subscriber_id,subscription_type,configuration,creation_date,active)
+VALUES (1,1,null,CURRENT_DATE(),true);
+
+INSERT INTO subscription (subscriber_id,subscription_type,configuration,creation_date,active)
+VALUES (1,2,null,CURRENT_DATE(),true);
+
+INSERT INTO subscription (subscriber_id,subscription_type,configuration,creation_date,active)
+VALUES (2,1,null,CURRENT_DATE(),true);
+
+INSERT INTO subscription (subscriber_id,subscription_type,configuration,creation_date,active)
+VALUES (3,1,null,CURRENT_DATE(),true);
+
+
+
